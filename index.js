da41f86b7c27a37bd3b3aeb015da0741ed5e1adf
klaraplivova <126472015+klaraplivova@users.noreply.github.com>
2024-05-19 15:53:13 +0200

    finished project03

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('funguju!');
+function updateSkill (id, number) {
+    let skill = document.querySelector(id)
+    let value = skill.querySelector(".skill__value")
+    let progress = skill.querySelector(".skill__progress")
+
+    value.textContent = number + " / 100"
+
+    progress.style.width = number + "%"
+}
+
+const html = prompt("Jak Vám jde HTML? (Zadejte číslo od 0 do 100)")
+const css = prompt("Jak Vám jde CSS? (Zadejte číslo od 0 do 100)")
+const js = prompt("Jak Vám jde JavaScript? (Zadejte číslo od 0 do 100)")
+
+updateSkill("#skill1", html)
+updateSkill("#skill2", css)
+updateSkill("#skill3", js)
